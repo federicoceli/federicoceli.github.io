41bf62182f31784613105bf69d9219bfdf2017e9
Federico <fed.celi@gmail.com>
2021-05-28 18:33:12 -0700

    minor

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "L’Operatore di Koopman - Parte 1",
-        "excerpt":"\\(\\gdef\\x{\\textcolor{#f68161}{x}}\\) \\(\\gdef\\g{\\textcolor{#7ed8ff}{g}}\\) Avere un sistema lineare tra le mani è il sogno di qualsiasi controllista. I sistemi lineari sono belli perché di loro sappiamo tutto: sappiamo come si evolveranno nel tempo e come risponderanno ai nostri stimoli. E quindi sappiamo alla perfezione che ingressi dare ad un sistema lineare affinché...","categories": [],
+        "excerpt":"Avere un sistema lineare tra le mani è il sogno di qualsiasi controllista. I sistemi lineari sono belli perché di loro sappiamo tutto: sappiamo come si evolveranno nel tempo e come risponderanno ai nostri stimoli. E quindi sappiamo alla perfezione che ingressi dare ad un sistema lineare affinché si comporti...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/koopman/",
         "teaser":null}]
